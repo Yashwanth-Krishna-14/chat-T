ad971f26e4cbfa9d781555a9f85293f8ed7b6db9
Yashwanth-Krishna-14 <152397326+Yashwanth-Krishna-14@users.noreply.github.com>
2026-05-07 21:52:00 +0530

    Refactoring

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,7 +13,7 @@ import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5001;
 const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" }));
@@ -28,14 +28,14 @@ app.use(
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
-
+/* 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
-}
+} */
 
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
