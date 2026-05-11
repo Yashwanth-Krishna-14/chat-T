610997690b9749ad909b46344c42fe29722a5eb0
Yashwanth-Krishna-14 <152397326+Yashwanth-Krishna-14@users.noreply.github.com>
2026-05-11 06:22:15 +0530

    Import error

--- backend/src/routes/conversation.route.js
+++ backend/src/routes/conversation.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getConversations } from "../controllers/conversation.controller.js";
+import { getConversations, createConversation } from "../controllers/conversation.controller.js";
 
 const router = express.Router();
 
