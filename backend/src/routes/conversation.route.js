2cc391289a71e7d312091f1e799058dcb95f233f
Yashwanth-Krishna-14 <152397326+Yashwanth-Krishna-14@users.noreply.github.com>
2026-05-11 06:24:30 +0530

    Fix

--- backend/src/routes/conversation.route.js
+++ backend/src/routes/conversation.route.js
@@ -1,10 +1,10 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getConversations, createConversation } from "../controllers/conversation.controller.js";
+import { createConversation } from "../controllers/conversation.controller.js";
 
 const router = express.Router();
 
-router.get("/", protectRoute, getConversations);
+//router.get("/", protectRoute, getConversations);
 
 // create/find 1-to-1 conversation
 router.post("/:userId", protectRoute, createConversation);
